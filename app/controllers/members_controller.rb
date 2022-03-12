b76e83867141c63032ca1d5e71a3e99b8672f4ec
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-03-12 12:00:37 +0100

    rails api created w/ devise & jwt config

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,21 @@
+class MembersController < ApplicationController
+  before_action :au 
+
+	def show
+	  user = get_user_from_token
+  	render json: {
+      message: "If you see this, you're in!",
+      user: user
+  	}
+  end
+
+  private
+
+  def get_user_from_token
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+    ENV['DEVISE_JWT_SECRET_KEY']).first
+    user_id = jwt_payload['sub']
+    User.find(user_id.to_s)
+  end
+
+end
