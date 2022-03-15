677da3f4e5269843b8e5a08839fcbb426a11c9db
Istarengwa <muller.lilian@outlook.fr>
2022-03-15 18:47:27 +0100

    add forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   devise_for :users,
             controllers: {
                 sessions: 'users/sessions',
-                registrations: 'users/registrations'
+                registrations: 'users/registrations',
+                passwords: 'users/passwords'
             },
             defaults: { format: :json }
   get '/member-data', to: 'members#show'
+  resource :passwords, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
