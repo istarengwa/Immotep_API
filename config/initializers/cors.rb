cb193d7832e12b95237eff3c1c49d8f4e31242bf
Istarengwa <muller.lilian@outlook.fr>
2022-03-18 13:29:29 +0100

    add security jwt and adresse for mailers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://immotep.vercel.app/', 'http://localhost:3000'
 
     resource '*',
-             headers: :any,
+             headers: %w[Authorization],
              methods: %i[get post put patch delete options head],
-             expose: %w[Authorization Uid]
+             expose: %w[Authorization],
+             max_age: 600
   end
 end
