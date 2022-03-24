8e11af4e2d280dbf4a066838225a4cf13e1c41f2
Istarengwa <muller.lilian@outlook.fr>
2022-03-24 13:22:51 +0100

    add test rspec for project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -25,4 +25,15 @@ RSpec.describe Project, type: :model do
     expect(Project.count).to eq(2)
   end
 
+  context 'housing association' do 
+    it 'add one hounsing in one project' do
+      user = User.create(email: "rspec@rspec.test", password: "password")
+      project = Project.create(title: "Test rspec", user_id: user.id)
+      project.housings << Housing.new(ad_price: 5, project_id: project.id)
+      project.save
+      expect(Housing.count).to eq(1)
+      expect(project.housings.count).to eq(1)
+    end
+  end
+
 end
